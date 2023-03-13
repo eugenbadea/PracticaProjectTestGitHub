2fec9bf2a29f60f87f6cff60f3af9e8318f43f79
EUGEN2037 <badea4@gmail.com>
2023-03-13 16:30:46 +0200

    Data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello titi aur!");
+    }
+    public static void printPeriodicallyCurrentDateAndTime(){
+        while(true){
+            try{
+                System.out.println(Instant.now());
+                Thread.sleep(5000);
+            }catch (InterruptedException e){
+                throw new RuntimeException(e);
+
+
+            }
+        }
     }
 }
