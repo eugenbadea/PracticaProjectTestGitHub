6761615df4fddc9b47206b0239573ae870fb29e5
EUGEN2037 <badea4@gmail.com>
2023-03-13 16:35:17 +0200

    Data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello titi aur!");
+        printPeriodicallyCurrentDateAndTime();
     }
     public static void printPeriodicallyCurrentDateAndTime(){
         while(true){
